f2de72a4a29b81869f2ff69904b88383b81b7d17
Mads Hartwich <s235102@dtu.dk>
2025-02-17 15:08:42 +0100

    added missingLink charms data

--- main/Data/missingLinkCharms/missingLinkCharmsData.js
+++ main/Data/missingLinkCharms/missingLinkCharmsData.js
@@ -2,90 +2,90 @@ const missingLinkCharmsData = [
     {
         id: '',
         name: 'Charm | Lil Ava',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/missinglink/kc_missinglink_ava_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'High Grade',
+            color: '#4b69ff'
         },
         url: ''
     },
     {
         id: '',
         name: 'Charm | Lil Crass',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/missinglink/kc_missinglink_guerilla_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'High Grade',
+            color: '#4b69ff'
         },
         url: ''
     },
     {
         id: '',
         name: 'Charm | Pinch O Salt',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/missinglink/kc_missinglink_sam_salty_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'High Grade',
+            color: '#4b69ff'
         },
         url: ''
     },
     {
         id: '',
         name: 'Charm | Big Kev',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/missinglink/kc_missinglink_kev_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'High Grade',
+            color: '#4b69ff'
         },
         url: ''
     },
     {
         id: '',
         name: 'Charm | Hot Sauce',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/missinglink/kc_missinglink_sam_catchup_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'High Grade',
+            color: '#4b69ff'
         },
         url: ''
     },
     {
         id: '',
         name: 'Charm | Lil SAS',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/missinglink/kc_missinglink_sas_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'High Grade',
+            color: '#4b69ff'
         },
         url: ''
     },
     {
         id: '',
         name: 'Charm | Lil Sqautch',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/missinglink/kc_missinglink_bigfoot_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Remarkable',
+            color: '#8847ff'
         },
         url: ''
     },
     {
         id: '',
         name: 'Charm | Thats Bananas',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/missinglink/kc_missinglink_banana_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Remarkable',
+            color: '#8847ff'
         },
         url: ''
     },
     {
         id: '',
         name: 'Charm | Lil Sandy',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/missinglink/kc_missinglink_cat_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Remarkable',
+            color: '#8847ff'
         },
         url: ''
     }
