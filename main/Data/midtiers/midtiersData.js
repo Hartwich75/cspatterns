d58708d4c64d90c19ead679eaf445847695a6963
Mads Hartwich <s235102@dtu.dk>
2025-02-17 13:47:42 +0100

    added midtier skin data

--- main/Data/midtiers/midtiersData.js
+++ main/Data/midtiers/midtiersData.js
@@ -1,31 +1,31 @@
 const midtiersData = [
     {
-        id: '',
+        id: 'skin-1576268',
         name: 'UMP-45 | Moonrise',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_ump45_aa_ump45_moonrise_sunset_light_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Restricted',
+            color: '#8847ff'
         },
         url: ''
     },
     {
-        id: '',
+        id: 'skin-1115096',
         name: 'Mac-10 | Amber Fade',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_mac10_aa_fade_metallic_light_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Mil-Spec Grade',
+            color: '#4b69ff'
         },
         url: ''
     },
     {
-        id: '',
+        id: 'skin-1901528',
         name: 'Sawed-Off | Amber Fade',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_sawedoff_aa_fade_metallic_light_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Mil-Spec Grade',
+            color: '#4b69ff'
         },
         url: ''
     }
