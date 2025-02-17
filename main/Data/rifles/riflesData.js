a1da0e07d26d9fa55590306e71bf5d5a7c0d485d
Mads Hartwich <s235102@dtu.dk>
2025-02-17 13:33:52 +0100

    added rifle skin data

--- main/Data/rifles/riflesData.js
+++ main/Data/rifles/riflesData.js
@@ -2,70 +2,70 @@ const riflesData = [
     {
         id: '',
         name: 'M4A1-S | Fade',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Covert',
+            color: '#eb4b4b'
         },
         url: ''
     },
     {
         id: '',
         name: 'Galil AR | Amber Fade',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_galilar_aa_fade_metallic_light_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Mil-Spec Grade',
+            color: '#4b69ff'
         },
         url: ''
     },
     {
-        id: '',
+        id: 'skin-525272',
         name: 'AUG | Amber Fade',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_aug_aa_fade_metallic_light_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Mil-Spec Grade',
+            color: '#4b69ff'
         },
         url: ''
     },
     {
-        id: '',
+        id: 'skin-592696',
         name: 'AWP | Paw',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_awp_am_awp_pawpaw_light_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Restricted',
+            color: '#8847ff'
         },
         url: ''
     },
     {
-        id: '',
+        id: 'skin-590732',
         name: 'AWP | Electric Hive',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_awp_hy_hive_light_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Classified',
+            color: '#d32ce6'
         },
         url: ''
     },
     {
-        id: '',
+        id: 'skin-593928',
         name: 'AWP | Fade',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_awp_aa_awp_fade_light_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Covert',
+            color: '#eb4b4b'
         },
         url: ''
     },
     {
-        id: '',
+        id: 'skin-590520',
         name: 'AWP | Boom',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/',
+        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/default_generated/weapon_awp_hy_blam_simple_light_png.png',
         rarity: {
-            name: '',
-            color: ''
+            name: 'Classified',
+            color: '#d32ce6'
         },
         url: ''
     }
