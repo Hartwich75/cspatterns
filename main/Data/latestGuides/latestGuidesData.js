1ee610e80973a44afb66dc73cd00370ac8114d25
Mads Hartwich <madshartwich@gmail.com>
2025-04-02 01:40:27 +0200

    added xm1014 solitude pattern guide

--- main/Data/latestGuides/latestGuidesData.js
+++ main/Data/latestGuides/latestGuidesData.js
@@ -1,6 +1,16 @@
 const latestGuidesData = [
     {
         id: '',
+        name: 'XM1014 | Solitude',
+        image: '/main/images/midtierPatterns/XM1014SolitudePattern95.png',
+        rarity: {
+            name: 'Restricted',
+            color: '#8847ff'
+        },
+        url: '/main/Views/midtiers/xm1014_solitude.html'
+    },
+    {
+        id: '',
         name: 'Charm | Titeenium AWP',
         image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/weapon_1/kc_wpn_awp_spoon_png.png',
         rarity: {
@@ -18,15 +28,5 @@ const latestGuidesData = [
             color: '#8847ff'
         },
         url: '/main/Views/smallArmsCharms/die_cast_ak_charm.html'
-    },
-    {
-        id: '',
-        name: 'Charm | Disco MAC',
-        image: 'https://raw.githubusercontent.com/Hartwich75/counter-strike-image-tracker/main/static/panorama/images/econ/keychains/weapon_1/kc_wpn_mac10_glitter_png.png',
-        rarity: {
-            name: 'Remarkable',
-            color: '#8847ff'
-        },
-        url: '/main/Views/smallArmsCharms/disco_mac_charm.html'
     }
 ];
--- main/Data/midtiers/midtiersData.js
+++ main/Data/midtiers/midtiersData.js
@@ -28,5 +28,15 @@ const midtiersData = [
             color: '#8847ff'
         },
         url: '/main/Views/midtiers/ump45_moonrise.html'
+    },
+    {
+        id: '',
+        name: 'XM1014 | Solitude',
+        image: '/main/images/midtierPatterns/XM1014SolitudePattern95.png',
+        rarity: {
+            name: 'Restricted',
+            color: '#8847ff'
+        },
+        url: '/main/Views/midtiers/xm1014_solitude.html'
     }
 ];
